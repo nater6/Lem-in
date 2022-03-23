119409deb90952ac8650b95c6ddc2571f5ac0917
Hannah.Alemu <hnnh.almu@gmail.com>
2022-03-23 13:12:42 +0000

    graph

--- main.go
+++ main.go
@@ -25,12 +25,82 @@ type Path struct {
 	start bool
 	final bool
 }
+//graph structre 
+// graph is adjacency list
+type Graph struct {
+	vertices []*Vertex
+
+}
+
+// vertex represents graph vertex
+type Vertex struct{
+	key int
+	adjacent []*Vertex
+}
+// add vertext
+func (g *Graph) AddVertex(k int) {
+	g.vertices = append(g.vertices, &Vertex{key: k})
+}
+//add edge
+
+func (g *Graph) AddEdge(from,to int) {
+	// get vertex 
+	fromVertex := g.getVertex(from)
+	toVertex:= g.getVertex(to)
+
+// check error
+if fromVertex == nil || toVertex == nil {
+		err := fmt.Errorf("invalid edge (%v ---> %v)", from, to)
+		fmt.Println(err.Error())
+} else if contains(fromVertex.adjacent, to) {
+	
+	} else {
+	//add edge
+		fromVertex.adjacent = append(fromVertex.adjacent, toVertex)
+}
+	
+
+
+}
+// get vertex
+func (g *Graph) getVertex(k int) *Vertex {
+	for i,v:= range g.vertices {
+		if v.key == k{
+			return g.vertices[i]
+		}
+	}
+	return nil
+}
+// contains
+func contains(s []*Vertex, k int) bool {
+			for _,v := range s {
+				if k== v.key {
+					return true 
+				}
+			}
+			return false 
+}
+// print will print the adjacent list for each vertex of the graph 
+func (g *Graph) Print() {
+	for _, v := range g.vertices {
+			fmt.Printf("\nVertex %v: ", v.key)
+			for _, v := range v.adjacent {
+				fmt.Printf("%v", v.key)
+			}
+	}
+}
 
 func main() {
 	Rooms, Links := SortFiles()
 	Links = FindDistances(Rooms, Links)
 	
-	fmt.Println(Links)
+	test := &Graph{}
+
+	for i:= 0; i<5; i++ {
+		test.AddVertex(i)
+	}
+	test.Print()
+	test.AddEdge(1,2)
 }
 
 func SortFiles() ([]Room, []Link){
