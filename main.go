7b353058105aa64647cdbc5d761ba54f6a06b560
Your Name <n.d.russell@hotmail.com>
2022-03-28 16:41:45 +0100

    dd

--- main.go
+++ main.go
@@ -19,11 +19,12 @@ type Graph struct {
 type Room struct {
 	Roomname string
 	adjacent map[string]*Room
+	visited	bool
 }
 
 // add vertext
 func (g *Graph) AddRoom(name string) {
-	g.Rooms = append(g.Rooms, &Room{Roomname: name, adjacent: make(map[string]*Room)})
+	g.Rooms = append(g.Rooms, &Room{Roomname: name, adjacent: make(map[string]*Room), visited: false})
 }
 
 //add edge
@@ -35,16 +36,30 @@ func (g *Graph) AddLinks(from, to string) {
 
 	// check
 	if fromRoom == nil || toRoom == nil {
+		//If either of the rooms dont exsist
 		err := fmt.Errorf("Room doesn't exsist (%v --- %v)", from, to)
 		fmt.Println(err.Error())
 	} else if contains(fromRoom.adjacent, to) || contains(toRoom.adjacent, from) {
-		err := fmt.Errorf(" Exsisting Link (%v --- %v)", from, to)
+		// if the link already exsists
+		err := fmt.Errorf(" Existing Link (%v --- %v)", from, to)
 		fmt.Println(err.Error())
-	} else {
-		//add edRoom.adjacent = apRoom.adjacent, toRoom)
+	} else if fromRoom == g.endRoom{
+		//Checking for the endroom
+		toRoom.adjacent[fromRoom.Roomname] = fromRoom
+	} else if toRoom == g.endRoom {
+		//Checking for the endroom
+		fromRoom.adjacent[toRoom.Roomname] = toRoom
+	} else if toRoom == g.startRoom {
+		//Checking for the startroom
+		toRoom.adjacent[fromRoom.Roomname] = fromRoom
+	} else if fromRoom == g.startRoom {
+		//Checking for the startroom
+		fromRoom.adjacent[toRoom.Roomname] = toRoom
+	} else if fromRoom != g.endRoom && toRoom != g.endRoom {
+		
 		fromRoom.adjacent[toRoom.Roomname] = toRoom
 		toRoom.adjacent[fromRoom.Roomname] = fromRoom
-	}
+	} 
 
 }
 
@@ -81,7 +96,42 @@ func main() {
 		fmt.Println(r.adjacent)
 		
 	}
+	
+	for _, v:= range roomList.Rooms {
+		x := *v
+		fmt.Print(x.Roomname + "|")
+	}
 
+}
+
+
+
+func PathFinder(g *Graph) [][]*Room {
+
+// 	//Begin from the start room and check which rooms it is linked to
+	start := g.startRoom
+	final := [][]*Room{}
+	path := []*Room{}
+
+	for start.adjacent != nil {
+		for i, v := range start.adjacent {
+			if !v.visited {
+				path = append(path, start.adjacent[i])
+				// 	//As each room is checked mark it as visited so it isn't checked again
+				start.visited = true
+				start = start.adjacent[i]
+			}
+		// if v == g.endRoom {
+		// 	final = append(final, path)
+		// } 
+	}
+	}
+	
+
+
+// 	//Once a path is complete, run again
+// 	// if the same path is found again,
+	return final
 }
 
 func SortFiles(g *Graph) {
@@ -93,7 +143,11 @@ func SortFiles(g *Graph) {
 
 	scanner.Split(bufio.ScanLines)
 
+	
+
 	for scanner.Scan() {
+		x := scanner.Text()
+		fmt.Println(x)
 
 		space := strings.Split(scanner.Text(), " ")
 
@@ -103,10 +157,10 @@ func SortFiles(g *Graph) {
 		}
 
 		if start {
-			g.startRoom = g.Rooms[i]
+			g.startRoom = g.Rooms[i-1]
 			start = false
 		} else if end {
-			g.endRoom = g.Rooms[i]
+			g.endRoom = g.Rooms[i-1]
 			end = false
 		}
 
@@ -115,14 +169,19 @@ func SortFiles(g *Graph) {
 			g.AddLinks(hyphen[0], hyphen[1])
 			
 		}
-	}
+	
 
-	if scanner.Text() == "##start" {
+	if x == "##start" {
 		start = true
+		fmt.Println("Start")
+		
 	}
 
-	if scanner.Text() == "##end" {
+	if x == "##end" {
 		end = true
 	}
 
 }
+
+}
+
